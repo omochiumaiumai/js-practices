8f9c1ccce62a03b6f652993330e61618003e668b
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-03-22 20:42:27 +0900

    specified_dateからcalendar_dateに変数名を変更

--- 02.calendar/js-calendar.js
+++ 02.calendar/js-calendar.js
@@ -6,13 +6,13 @@ const argv = require("minimist")(process.argv.slice(2));
 const year = argv.y || dayjs().year();
 const month = argv.m || dayjs().month() + 1;
 
-const specified_date = dayjs(`${year}-${month}-01`);
+const calendar_date = dayjs(`${year}-${month}-01`);
 
-console.log(`     ${specified_date.format("M月 YYYY")}     `);
+console.log(`     ${calendar_date.format("M月 YYYY")}     `);
 console.log("日 月 火 水 木 金 土");
 
-const first_day_of_week = specified_date.day();
-const last_day_of_month = specified_date.endOf("month").date();
+const first_day_of_week = calendar_date.day();
+const last_day_of_month = calendar_date.endOf("month").date();
 
 let week = "";
 for (let cnt = 0; cnt < first_day_of_week; cnt++) {
