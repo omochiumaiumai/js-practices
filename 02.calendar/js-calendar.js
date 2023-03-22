619d577f1a3264c17019d2f8b41fa4be89144131
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-03-22 20:41:20 +0900

    日付表示の繰り返し処理を追加

--- 02.calendar/js-calendar.js
+++ 02.calendar/js-calendar.js
@@ -10,3 +10,26 @@ const specified_date = dayjs(`${year}-${month}-01`);
 
 console.log(`     ${specified_date.format("M月 YYYY")}     `);
 console.log("日 月 火 水 木 金 土");
+
+const first_day_of_week = specified_date.day();
+const last_day_of_month = specified_date.endOf("month").date();
+
+let week = "";
+for (let cnt = 0; cnt < first_day_of_week; cnt++) {
+  week += "   ";
+}
+for (let day = 1; day <= last_day_of_month; day++) {
+  if (day < 10) {
+    week += ` ${day} `;
+  } else {
+    week += `${day} `;
+  }
+
+  if ((first_day_of_week + day) % 7 === 0) {
+    console.log(week);
+    week = "";
+  }
+}
+if (week.length > 0) {
+  console.log(week);
+}
