df99ceb7df4409614d6bf5bb8788d0a3c3b11027
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-03-26 16:01:43 +0900

    等価演算子(==)を厳密等価演算子(===)に修正

--- 01.fizzbuzz/js-fizzbuzz.js
+++ 01.fizzbuzz/js-fizzbuzz.js
@@ -1,11 +1,11 @@
 "use strict";
 
 for (let cnt = 1; cnt <= 20; cnt++) {
-  if (cnt % 3 == 0 && cnt % 5 == 0) {
+  if (cnt % 3 === 0 && cnt % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (cnt % 3 == 0) {
+  } else if (cnt % 3 === 0) {
     console.log("Fizz");
-  } else if (cnt % 5 == 0) {
+  } else if (cnt % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(cnt);
